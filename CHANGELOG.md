b5d4594806217bc5f4a81b4ff8ab05409faaf8f6
mmarchal <marchal.maxime@live.fr>
2023-10-09 17:18:16 +0200

    feat(login): Add login page

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+## 0.0.1
+
+_Feat_
+
+- [Issue #2](https://github.com/XPEHO/XpeApp/issues/2) Add the login page
+
+_Chore_
+
+- Init the repository and the project
---
+++ app/src/main/java/com/xpeho/xpeapp/ui/theme/Type.kt
@@ -0,0 +1,23 @@
+package com.xpeho.xpeapp.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.xpeho.xpeapp.R
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    body1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    )
+)
+
+val SfPro = FontFamily(
+    Font(R.font.sf_pro),
+)

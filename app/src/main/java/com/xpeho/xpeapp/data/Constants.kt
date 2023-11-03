f88523f1cb94d313fdade69b72814f08549f1c51
mmarchal <marchal.maxime@live.fr>
2023-11-03 16:57:19 +0100

    feat(featureFlipping): Add feature flipping gesture

--- app/src/main/java/com/xpeho/xpeapp/data/Constants.kt
+++ app/src/main/java/com/xpeho/xpeapp/data/Constants.kt
@@ -1,3 +1,12 @@
 package com.xpeho.xpeapp.data
 
 const val NEWSLETTERS_COLLECTION = "newsletters"
+const val FEATURE_FLIPPING_COLLECTION = "featureFlipping"
+
+enum class FeatureFlippingEnum{
+    newsletters,
+    vacation,
+    cra,
+    expenseReport,
+    colleagues,
+}
--- app/src/main/java/com/xpeho/xpeapp/presentation/page/HomePage.kt
+++ app/src/main/java/com/xpeho/xpeapp/presentation/page/HomePage.kt
@@ -1,55 +1,58 @@
 package com.xpeho.xpeapp.presentation.page
 
+import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Logout
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.ColorMatrix
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.xpeho.xpeapp.R
+import com.xpeho.xpeapp.data.FeatureFlippingEnum
 import com.xpeho.xpeapp.presentation.Resources
 import com.xpeho.xpeapp.presentation.componants.AppBar
 import com.xpeho.xpeapp.presentation.componants.Card
+import com.xpeho.xpeapp.presentation.viewModel.FeatureFlippingComposable
+import com.xpeho.xpeapp.presentation.viewModel.FeatureFlippingViewModel
 
 @Composable
 fun HomePage(
     onBackPressed: () -> Unit,
     navigationController : NavController,
+    featureFlippingViewModel: FeatureFlippingViewModel,
 ) {
-    Scaffold(
-        topBar = {
-            AppBar(
-                imageVector = Icons.AutoMirrored.Filled.Logout,
-                title = null,
-            ) {
-                onBackPressed()
-            }
+    Column {
+        AppBar(
+            imageVector = Icons.AutoMirrored.Filled.Logout,
+            title = null,
+        ) {
+            onBackPressed()
         }
-    ) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(32.dp),
             verticalArrangement = Arrangement.Center,
         ) {
-            Card(
-                imageResource = R.drawable.newsletters,
-                title = "Newsletters",
-                color = colorResource(id = R.color.xpeho_color),
-                redirection = {
-                    navigationController.navigate(route = "Newsletters")
-                },
-            )
+            FeatureFlippingComposable(
+                featureId = FeatureFlippingEnum.newsletters.name,
+                viewModel = featureFlippingViewModel,
+            ) {
+                Card(
+                    imageResource = R.drawable.newsletters,
+                    title = "Newsletters",
+                    color = colorResource(id = R.color.xpeho_color),
+                    redirection = {
+                        navigationController.navigate(route = "Newsletters")
+                    },
+                )
+            }
             LazyVerticalGrid(
                 verticalArrangement = Arrangement.spacedBy(20.dp),
                 horizontalArrangement = Arrangement.spacedBy(20.dp),

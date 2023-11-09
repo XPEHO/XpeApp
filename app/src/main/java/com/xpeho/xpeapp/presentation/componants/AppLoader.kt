38ecc4716ca4f119c8a2fe25f9f4789e0e2c2e3c
mmarchal <marchal.maxime@live.fr>
2023-11-09 16:59:47 +0100

    feat(tests): Add units tests

--- app/src/main/java/com/xpeho/xpeapp/presentation/componants/AppLoader.kt
+++ app/src/main/java/com/xpeho/xpeapp/presentation/componants/AppLoader.kt
@@ -2,12 +2,12 @@ package com.xpeho.xpeapp.presentation.componants
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -20,6 +20,7 @@ fun AppLoader() {
             modifier = Modifier
                 .size(48.dp)
                 .align(Alignment.Center)
+                .testTag("circularProgressIndicator")
         )
     }
 }

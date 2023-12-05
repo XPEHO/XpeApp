60ebc6e15d109fb59eb27b525e43262f278153f2
mmarchal <marchal.maxime@live.fr>
2023-12-05 11:08:14 +0100

    chore(cicd): Replace fix/android to main in Android YAML

--- app/src/main/java/com/xpeho/xpeapp/data/service/WordpressService.kt
+++ app/src/main/java/com/xpeho/xpeapp/data/service/WordpressService.kt
@@ -12,8 +12,6 @@ import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
 
-// private const val baseUrl = "http://yaki.uat.xpeho.fr:7830/wp-json/api/"
-//                           http://yaki.uat.xpeho.fr:7830/wp-json/api/v1/token
 private const val BASE_URL = BuildConfig.BACKEND_URL
 
 private val gson = GsonBuilder()

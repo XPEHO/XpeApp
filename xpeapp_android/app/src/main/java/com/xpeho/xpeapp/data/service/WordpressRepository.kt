41eddbf750aaa27a9e440ee25901bc8d241ee609
Loucas Cubeddu <lcubeddu@outlook.fr>
2024-04-02 11:07:53 +0200

    feat(AutoLogin): Remove magic numbers in WordpressRepository

--- xpeapp_android/app/src/main/java/com/xpeho/xpeapp/data/service/WordpressRepository.kt
+++ xpeapp_android/app/src/main/java/com/xpeho/xpeapp/data/service/WordpressRepository.kt
@@ -10,6 +10,10 @@ import java.net.UnknownHostException
 import javax.net.ssl.SSLHandshakeException
 
 class WordpressRepository {
+    companion object {
+        private const val HTTPBADREQUEST = 400
+        private const val HTTPSERVICEUNAVAILABLE = 503
+    }
 
     suspend fun authenticate(credentials: AuthentificationBody): AuthResult<WordpressToken> {
         return try {
@@ -43,7 +47,8 @@ class WordpressRepository {
             Log.e("WordpressRepository", "Network error: ${e.message}")
             return AuthResult.NetworkError
         }
-        if (e is HttpException && e.code() == 400)
+        // Backend sends a 400 Bad Request, should send a 401 Unauthorized
+        if (e is HttpException && e.code() == HTTPBADREQUEST)
             return AuthResult.Unauthorized
         Log.e("WordpressRepository", "Unknown error: ${e.message}")
         throw e
@@ -55,7 +60,7 @@ class WordpressRepository {
             is SocketTimeoutException -> true
             is SSLHandshakeException -> true
             is ConnectException -> true
-            is HttpException -> e.code() == 503
+            is HttpException -> e.code() == HTTPSERVICEUNAVAILABLE
             else -> false
         }
     }

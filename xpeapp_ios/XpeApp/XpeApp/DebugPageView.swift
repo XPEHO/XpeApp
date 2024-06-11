e341e40b385369f66e083ec4cc3d104c44e32e3b
Loucas Cubeddu <lcubeddu@outlook.fr>
2024-06-11 14:27:05 +0200

    feat(iOSQvstList): Display the list of qvst campaigns

--- xpeapp_ios/XpeApp/XpeApp/DebugPageView.swift
+++ xpeapp_ios/XpeApp/XpeApp/DebugPageView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct DebugPageView: View {
     @State var newsletterFetch = ""
     @State var qvstListFetch = ""
-
+    
     var body: some View {
         ScrollView {
             VStack {
@@ -27,16 +27,26 @@ struct DebugPageView: View {
                         }
                     }
                 }
+                SectionView(
+                    title: "QVST List fetch",
+                    codeBlock:qvstListFetch)
+                .onTapGesture {
+                    Task  {
+                        if let ql = await QvstCampaign.fetchActive()  {
+                            qvstListFetch = String(describing: ql)
+                        }
+                    }
+                }
                 Spacer()
             }
             .padding()
         }
     }
-
+    
     private struct SectionView: View {
         var title: String
         var codeBlock: String
-
+        
         var body: some View {
             HStack{
                 Text(title)
@@ -54,6 +64,34 @@ struct DebugPageView: View {
             }
         }
     }
+    
+    private func fetchQvstList() async {
+        let urlStr = "http://yaki.uat.xpeho.fr:7830/wp-json/xpeho/v1/qvst/campaigns:active"
+        guard let url = URL(string: urlStr) else {
+            print("Malformed url \(urlStr)")
+            return
+        }
+        do {
+            let data = try await URLSession.shared.data(from: url).0
+            do {
+                guard let json = try JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] else {
+                    print("Error parsing json")
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    qvstListFetch = String(describing: json)
+                    print("fetch: \(qvstListFetch)")
+                }
+            } catch {
+                print("Error parsing json: \(error)")
+            }
+        } catch {
+            print("Error trying to retrieve list of qvst: \(error)")
+            return
+        }
+    }
+
 }
 
 #Preview {
---
+++ xpeapp_ios/XpeApp/XpeApp/QvstCampaigns/QvstCampaign.swift
@@ -0,0 +1,88 @@
+//
+//  QvstCampaign.swift
+//  XpeApp
+//
+//  Created by Loucas Cubeddu on 07/06/2024.
+//
+
+import Foundation
+
+struct QvstTheme: Codable {
+    let id: String
+    let name: String
+}
+
+struct QvstCampaign: Codable, Identifiable {
+    let id: String
+    let name: String
+    let theme: QvstTheme
+    let status: String
+    let startDate: Date
+    let endDate: Date
+    let participationRate: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case theme
+        case status
+        case startDate = "start_date"
+        case endDate = "end_date"
+        case participationRate = "participation_rate"
+    }
+}
+
+extension QvstCampaign{
+    // Note(Loucas): This init is needed to decode Dates
+    static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        return formatter
+    }()
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.theme = try container.decode(QvstTheme.self, forKey: .theme)
+        self.status = try container.decode(String.self, forKey: .status)
+        self.participationRate = try container.decode(String.self, forKey: .participationRate)
+        
+        let startDateString = try container.decode(String.self, forKey: .startDate)
+        let endDateString = try container.decode(String.self, forKey: .endDate)
+        
+        guard let startDate = QvstCampaign.dateFormatter.date(from: startDateString),
+              let endDate = QvstCampaign.dateFormatter.date(from: endDateString) else {
+            throw DecodingError.dataCorruptedError(forKey: .startDate, in: container, debugDescription: "Date string does not match format expected by formatter.")
+        }
+        
+        self.startDate = startDate
+        self.endDate = endDate
+    }
+    
+    
+    static func fetchActive() async -> [QvstCampaign]? {
+        let endpointUrl = "http://yaki.uat.xpeho.fr:7830/wp-json/xpeho/v1/qvst/campaigns:active"
+        guard let url = URL(string: endpointUrl) else {
+            print("Malformed url \(endpointUrl)")
+            return nil
+        }
+        let data: Data
+        do {
+            data = try await URLSession.shared.data(from: url).0
+        } catch {
+            print("Error trying to retrieve list of qvst: \(error)")
+            return nil
+        }
+        let decode: [QvstCampaign]
+        do {
+            decode = try JSONDecoder().decode([QvstCampaign].self, from: data)
+        } catch {
+            print("Error parsing json: \(error)")
+            return nil
+        }
+        
+        return decode
+    }
+}

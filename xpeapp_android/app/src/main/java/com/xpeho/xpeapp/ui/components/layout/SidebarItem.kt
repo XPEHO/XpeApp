26a9f62de2abaf193c26beb012694ddd00e0311f
NyraSama <debouvriesryan@gmail.com>
2024-10-31 11:00:28 +0100

    feat(android): Add copyright and version code

--- xpeapp_android/app/src/main/java/com/xpeho/xpeapp/ui/components/layout/SidebarItem.kt
+++ xpeapp_android/app/src/main/java/com/xpeho/xpeapp/ui/components/layout/SidebarItem.kt
@@ -6,16 +6,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.xpeho.xpeho_ui_android.foundations.Fonts as XpehoFonts
 
 @Composable
 fun SidebarItem(
@@ -41,12 +37,6 @@ fun SidebarItem(
             modifier = Modifier
                 .width(8.dp)
         )
-        Text(
-            label,
-            fontSize = 18.sp,
-            fontFamily = XpehoFonts.raleway,
-            fontWeight = FontWeight.Bold,
-            color = Color.White
-        )
+        Subtitle(label = label)
     }
 }

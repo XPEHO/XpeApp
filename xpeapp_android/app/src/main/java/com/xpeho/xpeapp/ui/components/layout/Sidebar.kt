049cb247d30c4bfad7c1c3930ebbafbcfce7731f
NyraSama <debouvriesryan@gmail.com>
2024-10-31 11:00:28 +0100

    feat(android): Add copyright and version code

--- xpeapp_android/app/src/main/java/com/xpeho/xpeapp/ui/components/layout/Sidebar.kt
+++ xpeapp_android/app/src/main/java/com/xpeho/xpeapp/ui/components/layout/Sidebar.kt
@@ -1,5 +1,7 @@
 package com.xpeho.xpeapp.ui.components.layout
 
+import android.content.Intent
+import android.net.Uri
 import android.widget.Toast
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.animateFloatAsState
@@ -9,19 +11,33 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.xpeho.xpeapp.BuildConfig
 import com.xpeho.xpeapp.R
 import com.xpeho.xpeapp.XpeApp
 import com.xpeho.xpeapp.data.FeatureFlippingEnum
@@ -29,12 +45,12 @@ import com.xpeho.xpeapp.enums.Screens
 import com.xpeho.xpeapp.ui.Resources
 import com.xpeho.xpeapp.ui.viewModel.FeatureFlippingUiState
 import com.xpeho.xpeapp.ui.viewModel.FeatureFlippingViewModel
+import com.xpeho.xpeho_ui_android.ClickyButton
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import com.xpeho.xpeho_ui_android.R.drawable as XpehoRes
 import com.xpeho.xpeho_ui_android.foundations.Colors as XpehoColors
-import com.xpeho.xpeho_ui_android.ClickyButton
 
 @Composable
 fun Sidebar(
@@ -127,26 +143,54 @@ fun Sidebar(
                         }
                     }
                 }
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    ClickyButton(
-                        label = "Se déconnecter",
-                        size = 16.sp,
-                        verticalPadding = 5.dp,
-                        horizontalPadding = 15.dp,
-                        backgroundColor = Color.White,
-                        labelColor = XpehoColors.CONTENT_COLOR
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
                     ) {
-                        CoroutineScope(Dispatchers.IO).launch {
-                            XpeApp.appModule.authenticationManager.logout()
+                        ClickyButton(
+                            label = "Se déconnecter",
+                            size = 16.sp,
+                            verticalPadding = 5.dp,
+                            horizontalPadding = 15.dp,
+                            backgroundColor = Color.White,
+                            labelColor = XpehoColors.CONTENT_COLOR
+                        ) {
+                            CoroutineScope(Dispatchers.IO).launch {
+                                XpeApp.appModule.authenticationManager.logout()
+                            }
+                            // Return to login page and clear the backstack
+                            navigationController.navigate(route = Screens.Login.name) {
+                                popUpTo(Screens.Home.name) { inclusive = true }
+                            }
                         }
-                        // Return to login page and clear the backstack
-                        navigationController.navigate(route = Screens.Login.name) {
-                            popUpTo(Screens.Home.name) { inclusive = true }
-                        }
+                    }
+                    HorizontalDivider(
+                        color = Color.White,
+                        thickness = 1.dp,
+                        modifier = Modifier
+                            .fillMaxWidth(0.7f)
+                            .padding(bottom = 10.dp, top = 20.dp)
+                    )
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        val versionCode = BuildConfig.VERSION_NAME
+                        Subtitle(
+                            label = "by XPEHO",
+                            modifier = Modifier
+                                .clickable {
+                                    // Open the XPEHO website
+                                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.xpeho.com"))
+                                    context.startActivity(intent)
+                                }
+                        )
+                        Subtitle(label = "v$versionCode")
                     }
                 }
             }
--- xpeapp_android/app/src/main/java/com/xpeho/xpeapp/ui/components/layout/Subtitle.kt
+++ xpeapp_android/app/src/main/java/com/xpeho/xpeapp/ui/components/layout/Subtitle.kt
@@ -1,26 +1,23 @@
 package com.xpeho.xpeapp.ui.components.layout
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.xpeho.xpeho_ui_android.foundations.Colors as XpehoColors
 import com.xpeho.xpeho_ui_android.foundations.Fonts as XpehoFonts
 
 @Composable
-fun Title(
-    label: String
+fun Subtitle(
+    label: String,
+    modifier: androidx.compose.ui.Modifier = androidx.compose.ui.Modifier
 ) {
     Text(
         text = label,
         fontSize = 18.sp,
         fontFamily = XpehoFonts.raleway,
         fontWeight = FontWeight.Bold,
-        color = XpehoColors.CONTENT_COLOR,
-        modifier = Modifier
-            .padding(bottom = 15.dp)
+        color = Color.White,
+        modifier = modifier
     )
 }

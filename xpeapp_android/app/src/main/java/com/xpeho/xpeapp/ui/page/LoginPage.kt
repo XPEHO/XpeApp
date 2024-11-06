4b8c3ab610f1423b7d88f9e2cc358d9be342b11a
NyraSama <debouvriesryan@gmail.com>
2024-11-06 11:02:55 +0100

    fix(android): Reduce cognitive complexity of loginPage

--- xpeapp_android/app/src/main/java/com/xpeho/xpeapp/ui/page/LoginPage.kt
+++ xpeapp_android/app/src/main/java/com/xpeho/xpeapp/ui/page/LoginPage.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
@@ -94,84 +95,169 @@ private fun LoginPageContent(
 
     Scaffold(
         content = {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(24.dp),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.app_icon_cropped),
-                    tint = XpehoColors.XPEHO_COLOR,
-                    contentDescription = stringResource(id = R.string.xpeho_logo_content),
-                    modifier = Modifier
-                        .width(200.dp)
-                )
-                Spacer(modifier = Modifier.height(100.dp))
-                InputText(
-                    label = stringResource(id = R.string.login_page_email),
-                    defaultInput = usernameTextField,
-                    labelSize = 14.sp,
-                    inputSize = 18.sp,
-                    focusRequester = usernameFocusRequester,
-                    keyboardAction = ImeAction.Next,
-                    onKeyboardAction = {
-                        passwordFocusRequester.requestFocus()
-                    }
-                ) {
-                    usernameTextField = it
-                }
-                CheckInputField(errorTextFieldUser, R.string.login_page_enter_email_warning)
-                Spacer(modifier = Modifier.height(10.dp))
-                InputText(
-                    label = stringResource(id = R.string.login_page_password),
-                    defaultInput = passwordTextField,
-                    labelSize = 14.sp,
-                    inputSize = 18.sp,
-                    password = true,
-                    focusRequester = passwordFocusRequester,
-                    keyboardAction = ImeAction.Done,
-                    onKeyboardAction = {
-                        focusManager.clearFocus()
-                        errorTextFieldUser = false
-                        errorTextFieldPassword = false
-                        if (usernameTextField.isNotEmpty() && passwordTextField.isNotEmpty()) {
-                            onLoginPressed(usernameTextField, passwordTextField)
-                        } else {
-                            if (usernameTextField.isEmpty()) errorTextFieldUser = true
-                            if (passwordTextField.isEmpty()) errorTextFieldPassword = true
-                        }
-                    }
-                ) {
-                    passwordTextField = it
-                }
-                CheckInputField(errorTextFieldPassword, R.string.login_page_enter_password_warning)
-                Spacer(modifier = Modifier.height(32.dp))
-                ClickyButton(
-                    label = "SE CONNECTER",
-                    size = 18.sp,
-                    verticalPadding = 10.dp,
-                    horizontalPadding = 50.dp,
-                    backgroundColor = XpehoColors.XPEHO_COLOR,
-                    labelColor = Color.White,
-                    enabled = !(wordpressState is WordpressUiState.LOADING ||
-                            wordpressState is WordpressUiState.SUCCESS)
-                ) {
-                    errorTextFieldUser = false
-                    errorTextFieldPassword = false
-                    if (usernameTextField.isNotEmpty() && passwordTextField.isNotEmpty()) {
-                        onLoginPressed(usernameTextField, passwordTextField)
-                    } else {
-                        if (usernameTextField.isEmpty()) errorTextFieldUser = true
-                        if (passwordTextField.isEmpty()) errorTextFieldPassword = true
-                    }
-                }
-            }
+            LoginPageContentColumn(
+                usernameTextField,
+                passwordTextField,
+                errorTextFieldUser,
+                errorTextFieldPassword,
+                focusManager,
+                usernameFocusRequester,
+                passwordFocusRequester,
+                wordpressState,
+                onLoginPressed,
+                onUsernameChange = { usernameTextField = it },
+                onPasswordChange = { passwordTextField = it },
+                onErrorUserChange = { errorTextFieldUser = it },
+                onErrorPasswordChange = { errorTextFieldPassword = it }
+            )
         }
     )
+}
 
+@Composable
+private fun LoginPageContentColumn(
+    usernameTextField: String,
+    passwordTextField: String,
+    errorTextFieldUser: Boolean,
+    errorTextFieldPassword: Boolean,
+    focusManager: FocusManager,
+    usernameFocusRequester: FocusRequester,
+    passwordFocusRequester: FocusRequester,
+    wordpressState: WordpressUiState,
+    onLoginPressed: (username: String, password: String) -> Unit,
+    onUsernameChange: (String) -> Unit,
+    onPasswordChange: (String) -> Unit,
+    onErrorUserChange: (Boolean) -> Unit,
+    onErrorPasswordChange: (Boolean) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        LoginPageIcon()
+        Spacer(modifier = Modifier.height(100.dp))
+        LoginPageInputFields(
+            usernameTextField,
+            passwordTextField,
+            errorTextFieldUser,
+            errorTextFieldPassword,
+            focusManager,
+            usernameFocusRequester,
+            passwordFocusRequester,
+            onLoginPressed,
+            onUsernameChange,
+            onPasswordChange,
+            onErrorUserChange,
+            onErrorPasswordChange
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        LoginPageButton(
+            wordpressState,
+            usernameTextField,
+            passwordTextField,
+            onLoginPressed,
+            onErrorUserChange,
+            onErrorPasswordChange
+        )
+    }
+}
 
+@Composable
+private fun LoginPageIcon() {
+    Icon(
+        painter = painterResource(id = R.drawable.app_icon_cropped),
+        tint = XpehoColors.XPEHO_COLOR,
+        contentDescription = stringResource(id = R.string.xpeho_logo_content),
+        modifier = Modifier.width(200.dp)
+    )
+}
+
+@Composable
+private fun LoginPageInputFields(
+    usernameTextField: String,
+    passwordTextField: String,
+    errorTextFieldUser: Boolean,
+    errorTextFieldPassword: Boolean,
+    focusManager: FocusManager,
+    usernameFocusRequester: FocusRequester,
+    passwordFocusRequester: FocusRequester,
+    onLoginPressed: (username: String, password: String) -> Unit,
+    onUsernameChange: (String) -> Unit,
+    onPasswordChange: (String) -> Unit,
+    onErrorUserChange: (Boolean) -> Unit,
+    onErrorPasswordChange: (Boolean) -> Unit
+) {
+    InputText(
+        label = stringResource(id = R.string.login_page_email),
+        defaultInput = usernameTextField,
+        labelSize = 14.sp,
+        inputSize = 18.sp,
+        focusRequester = usernameFocusRequester,
+        keyboardAction = ImeAction.Next,
+        onKeyboardAction = {
+            passwordFocusRequester.requestFocus()
+        }
+    ) {
+        onUsernameChange(it)
+    }
+    CheckInputField(errorTextFieldUser, R.string.login_page_enter_email_warning)
+    Spacer(modifier = Modifier.height(10.dp))
+    InputText(
+        label = stringResource(id = R.string.login_page_password),
+        defaultInput = passwordTextField,
+        labelSize = 14.sp,
+        inputSize = 18.sp,
+        password = true,
+        focusRequester = passwordFocusRequester,
+        keyboardAction = ImeAction.Done,
+        onKeyboardAction = {
+            focusManager.clearFocus()
+            onErrorUserChange(false)
+            onErrorPasswordChange(false)
+            if (usernameTextField.isNotEmpty() && passwordTextField.isNotEmpty()) {
+                onLoginPressed(usernameTextField, passwordTextField)
+            } else {
+                if (usernameTextField.isEmpty()) onErrorUserChange(true)
+                if (passwordTextField.isEmpty()) onErrorPasswordChange(true)
+            }
+        }
+    ) {
+        onPasswordChange(it)
+    }
+    CheckInputField(errorTextFieldPassword, R.string.login_page_enter_password_warning)
+}
+
+@Composable
+private fun LoginPageButton(
+    wordpressState: WordpressUiState,
+    usernameTextField: String,
+    passwordTextField: String,
+    onLoginPressed: (username: String, password: String) -> Unit,
+    onErrorUserChange: (Boolean) -> Unit,
+    onErrorPasswordChange: (Boolean) -> Unit
+) {
+    ClickyButton(
+        label = "SE CONNECTER",
+        size = 18.sp,
+        verticalPadding = 10.dp,
+        horizontalPadding = 50.dp,
+        backgroundColor = XpehoColors.XPEHO_COLOR,
+        labelColor = Color.White,
+        enabled = !(wordpressState is WordpressUiState.LOADING ||
+                wordpressState is WordpressUiState.SUCCESS)
+    ) {
+        onErrorUserChange(false)
+        onErrorPasswordChange(false)
+        if (usernameTextField.isNotEmpty() && passwordTextField.isNotEmpty()) {
+            onLoginPressed(usernameTextField, passwordTextField)
+        } else {
+            if (usernameTextField.isEmpty()) onErrorUserChange(true)
+            if (passwordTextField.isEmpty()) onErrorPasswordChange(true)
+        }
+    }
 }
 
 @Composable

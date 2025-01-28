ed8bba2a4d7c3c3f1bbc12b4b7477e11e13f0904
Théo Lebègue <theo.lebegue@icloud.com>
2025-01-28 21:58:17 +0100

    feat(app_modal_android): adding the about part in sidebar

--- xpeapp_android/app/src/main/java/com/xpeho/xpeapp/ui/components/layout/Sidebar.kt
+++ xpeapp_android/app/src/main/java/com/xpeho/xpeapp/ui/components/layout/Sidebar.kt
@@ -1,8 +1,5 @@
 package com.xpeho.xpeapp.ui.components.layout
 
-import SidebarItemProfile
-import android.content.Intent
-import android.net.Uri
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
@@ -14,17 +11,14 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
@@ -32,34 +26,24 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.xpeho.xpeapp.BuildConfig
 import com.xpeho.xpeapp.R
 import com.xpeho.xpeapp.XpeApp
 import com.xpeho.xpeapp.enums.Screens
-import com.xpeho.xpeapp.ui.Measurements
 import com.xpeho.xpeapp.ui.Resources
-import com.xpeho.xpeho_ui_android.ClickyButton
-import com.xpeho.xpeho_ui_android.foundations.Fonts
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import com.xpeho.xpeho_ui_android.R.drawable as XpehoRes
 import com.xpeho.xpeho_ui_android.foundations.Colors as XpehoColors
 
 @Composable
 fun Sidebar(
     navigationController: NavController,
-    sidebarVisible: MutableState<Boolean>
+    sidebarVisible: MutableState<Boolean>,
+    showDialog: MutableState<Boolean>
 ) {
-    // Feature Flipping
-    val context = LocalContext.current
+
     val ffManager = XpeApp.appModule.featureFlippingManager
 
     // Computed sidebar size
@@ -79,7 +63,6 @@ fun Sidebar(
             .fillMaxHeight()
             .fillMaxWidth(sidebarWidth)
             .background(color = XpehoColors.XPEHO_COLOR)
-            // Prevent clickable element under the sidebar to be triggered on click
             .clickable(
                 indication = null,
                 interactionSource = remember { MutableInteractionSource() }
@@ -95,7 +78,6 @@ fun Sidebar(
                     .fillMaxHeight(),
                 verticalArrangement = Arrangement.SpaceBetween
             ) {
-
                 Column {
                     IconButton(
                         onClick = {
@@ -121,7 +103,7 @@ fun Sidebar(
                     Box(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .background(Color.Black.copy(alpha = 0.1f)) // Arrière-plan noir avec 10% d'opacité
+                            .background(Color.Black.copy(alpha = 0.1f))
                     ) {
                         SidebarItemProfile(
                             navigationController = navigationController,
@@ -133,15 +115,16 @@ fun Sidebar(
                             .height(20.dp)
                     )
 
-                    Column (
+                    Column(
                         modifier = Modifier
                             .padding(horizontal = 18.dp),
                     ) {
                         SidebarItem(
-                            navigationController = navigationController,
                             icon = painterResource(id = R.drawable.home),
                             label = "Accueil",
-                            route = Screens.Home.name
+                            onClick = {
+                                navigationController.navigate(Screens.Home.name)
+                            }
                         )
                         Spacer(
                             modifier = Modifier
@@ -150,91 +133,29 @@ fun Sidebar(
                         for (menuItem in Resources().listOfMenu) {
                             if (ffManager.isFeatureEnabled(menuItem.featureFlippingId)) {
                                 SidebarItem(
-                                    navigationController = navigationController,
                                     icon = painterResource(id = menuItem.idImage),
                                     label = menuItem.title,
-                                    route = menuItem.redirection
+                                    onClick = {
+                                        navigationController.navigate(menuItem.redirection)
+                                    }
                                 )
                                 Spacer(
                                     modifier = Modifier
                                         .height(20.dp)
                                 )
                             }
-                        } }
-
+                        }
+                        SidebarItem(
+                            icon = painterResource(id = R.drawable.about),
+                            label = stringResource(id = R.string.about_view_about_label),
+                            onClick = {
+                                sidebarVisible.value = false
+                                showDialog.value = true
+                            }
+                        )
+                    }
                 }
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier
-                        .padding(bottom = 20.dp, start = 18.dp, end = 18.dp),
-                ) {
-                    HorizontalDivider(
-                        color = Color.White,
-                        thickness = 1.dp,
-                        modifier = Modifier
-                            .fillMaxWidth(Measurements.SIDEBAR_DIVIDER_PERCENTAGE)
-                            .padding(bottom = 10.dp, top = 20.dp)
-                    )
-                    SidebarInfoSection(context)
-                    SidebarConfidentialityButton(context)
-                }
-
-
             }
         }
-    }
-}
-
-
-@Composable
-fun SidebarInfoSection(
-    context: android.content.Context
-) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        val versionCode = BuildConfig.VERSION_NAME
-        Subtitle(
-            label = "by XPEHO",
-            modifier = Modifier
-                .clickable {
-                    // Open the XPEHO website
-                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.xpeho.com"))
-                    context.startActivity(intent)
-                }
-        )
-        Subtitle(label = "v$versionCode")
-    }
-}
-
-@Composable
-fun SidebarConfidentialityButton(
-    context: android.content.Context
-) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = "Politiques de confidentialité",
-            fontSize = 14.sp,
-            fontFamily = Fonts.raleway,
-            fontWeight = FontWeight.Bold,
-            textDecoration = TextDecoration.Underline,
-            color = Color.White,
-            modifier = Modifier
-                .clickable {
-                    // Open the confidentiality page
-                    val intent =
-                        Intent(
-                            Intent.ACTION_VIEW,
-                            Uri.parse("https://github.com/XPEHO/XpeApp/blob/main/PRIVACY_POLICY.md")
-                        )
-                    context.startActivity(intent)
-                }
-        )
     }
 }
